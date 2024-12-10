0af46785ade9a42e1d12831e6fdddc30037b2c85
Stanisław Froń <stasiu.fron2003@gmail.com>
2024-12-10 19:11:10 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,7 +1,46 @@
 ﻿namespace Simulator;
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "Unknown";
     public uint Size { get; set; } = 3;
     public string Info => $"{Description} <{Size}>";
+     
+    public string Description
+    {
+        get => description;
+        init
+        {
+            var trimmedValue = value.Trim();
+
+            if (trimmedValue.Length < 3)
+            {
+                for (int i = trimmedValue.Length; i < 3; i++)
+                {
+                    trimmedValue += "#";
+                }
+            }
+
+            if (trimmedValue.Length > 15)
+            {
+                trimmedValue = trimmedValue.Substring(0, 15);
+            }
+
+            trimmedValue = trimmedValue.Trim();
+
+            if (trimmedValue.Length < 3)
+            {
+                for (int i = trimmedValue.Length; i < 3; i++)
+                {
+                    trimmedValue += "#";
+                }
+            }
+
+            if (char.IsLower(trimmedValue[0]))
+            {
+                trimmedValue = char.ToUpper(trimmedValue[0]) + trimmedValue.Substring(1);
+            }
+
+            description = trimmedValue;
+        }
+    }
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,8 +2,66 @@
 
 public class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
+    private string name = "Unknown";
+    public string Name
+    {
+        get => name;
+        init
+        {
+            var trimmedValue = value.Trim();
+
+            if (trimmedValue.Length < 3)
+            {
+                for (int i = trimmedValue.Length; i < 3; i++)
+                {
+                    trimmedValue += "#";
+                }
+            }
+
+            if (trimmedValue.Length > 25)
+            {
+                trimmedValue = trimmedValue.Substring(0, 25);
+            }
+
+            trimmedValue = trimmedValue.Trim();
+
+            if (trimmedValue.Length < 3)
+            {
+                for (int i = trimmedValue.Length; i < 3; i++)
+                {
+                    trimmedValue += "#";
+                }
+            }
+
+            if (char.IsLower(trimmedValue[0]))
+            {
+                trimmedValue = char.ToUpper(trimmedValue[0]) + trimmedValue.Substring(1);
+            }
+
+            name = trimmedValue;
+        }
+    }
+
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        init 
+        {
+            if (value < 1)
+            {
+                level = 1;
+            }
+            else if (value > 10)
+            {
+                level = 10;
+            }
+            else
+            {
+                level = value;
+            }
+        }
+    }
 
     public Creature(string name, int level = 1)
     {
@@ -16,4 +74,12 @@ public class Creature
     public string Info => $"{Name} [{Level}]";
 
     public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+
+    public void Upgrade()
+    {
+        if (Level < 10)
+        {
+            level++;
+        }
+    }
 }
